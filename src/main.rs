99507ffba7b54f61571412b21ba8baa6c7f5959a
Paulo Cabral <paulosanz@poli.ufrj.br>
2024-07-19 18:32:35 -0300

    Remove dir listing

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,13 @@
 fn main() {
+    let content = "123123123123123".repeat(1_000_000);
     loop {
-        let dir = std::fs::read_dir("/mnt").unwrap().map(|p| p.unwrap().path()).collect::<Vec<_>>();
+        let dir = std::fs::read_dir("/mnt")
+            .unwrap()
+            .map(|p| p.unwrap().path())
+            .collect::<Vec<_>>();
         let index = dir.len();
         println!("Write {index}");
-        println!("{:?}", dir);
-        std::fs::write(format!("/mnt/file-{index}.txt"), b"123123123123123").unwrap();
+        std::fs::write(format!("/mnt/file-{index}.txt"), content.as_bytes()).unwrap();
         std::thread::sleep(std::time::Duration::from_secs(5));
     }
 }
