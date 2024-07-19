0f8c9cfe8db7180f7a09c1fee1bce47a4048609d
Paulo Cabral <paulosanz@poli.ufrj.br>
2024-07-19 18:02:08 -0300

    Write last state

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,9 @@
 fn main() {
-    let mut index = 0;
     loop {
-        index += 1;
+        let dir = std::fs::read_dir("/mnt").unwrap().map(|p| p.unwrap().path()).collect::<Vec<_>>();
+        let index = dir.len();
         println!("Write {index}");
-        println!("{:?}", std::fs::read_dir("/mnt").unwrap().map(|p| p.unwrap().path()).collect::<Vec<_>>());
+        println!("{:?}", dir);
         std::fs::write(format!("/mnt/file-{index}.txt"), b"123123123123123").unwrap();
         std::thread::sleep(std::time::Duration::from_secs(5));
     }
