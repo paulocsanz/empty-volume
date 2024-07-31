159a4e8d8326b96f88303144f9f5f4b530a114e9
Paulo Cabral <paulosanz@poli.ufrj.br>
2024-07-31 19:07:58 -0300

    Remove file

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,15 @@
 fn main() {
-    let content = "123123123123123".repeat(10_000_000);
     loop {
         let Ok(dir) = std::fs::read_dir("/mnt") else {
             continue
         };
-        let dir = dir.map(|p| p.unwrap().path()).collect::<Vec<_>>();
         let index = dir.len();
-        println!("Write {index}");
-        if let Err(err) = std::fs::write(format!("/mnt/file-{index}.txt"), content.as_bytes()) {
-            println!("Error: {err}");
+        println!("Found {index}");
+
+        for path in dir {
+            let _ = std::fs::remove_file(path);
+            let _ = std::fs::remove_dir(path);
         }
-        std::thread::sleep(std::time::Duration::from_secs(1));
+        std::thread::sleep(std::time::Duration::from_secs(5));
     }
 }
