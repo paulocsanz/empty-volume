4e5b888ef123c2c0f3e3558b577d064d75288d68
Paulo Cabral <paulosanz@poli.ufrj.br>
2024-07-31 19:16:42 -0300

    Fix compilation

--- src/main.rs
+++ src/main.rs
@@ -3,13 +3,17 @@ fn main() {
         let Ok(dir) = std::fs::read_dir("/mnt") else {
             continue
         };
-        let index = dir.len();
-        println!("Found {index}");
+        let mut count = 0;
 
-        for path in dir {
-            let _ = std::fs::remove_file(path);
+        for entry in dir {
+            let Ok(path) = entry.map(|p| p.path()) else {
+                continue
+            };
+            let _ = std::fs::remove_file(&path);
             let _ = std::fs::remove_dir(path);
+            count += 1;
         }
+        println!("Found {count}");
         std::thread::sleep(std::time::Duration::from_secs(5));
     }
 }
