0b0872cf76e45ac26389146ff7f0e1528ad2bdfc
Paulo Cabral <paulosanz@poli.ufrj.br>
2024-07-31 19:33:39 -0300

    Add logs

--- src/main.rs
+++ src/main.rs
@@ -6,11 +6,11 @@ fn main() {
         let mut count = 0;
 
         for entry in dir {
-            let Ok(path) = entry.map(|p| p.path()) else {
+            let Ok(path) = entry.map(|p| dbg!(p.path())) else {
                 continue
             };
-            let _ = std::fs::remove_file(&path);
-            let _ = std::fs::remove_dir(path);
+            let _ = dbg!(std::fs::remove_file(&path));
+            let _ = dbg!(std::fs::remove_dir(path));
             count += 1;
         }
         println!("Found {count}");
